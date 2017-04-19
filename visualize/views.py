a311291c8fd1e60e9009bdbd37daaf963e88cf8d
Abeneazer Chafamo <achafamo@users.noreply.github.com>
2017-04-19 18:51:17 -0400

    Update views.py

--- visualize/views.py
+++ visualize/views.py
@@ -9,32 +9,41 @@ import os
 
 import collections
 def home(request):
+    '''
+    this function returns our main page which displays the visualization and grouping of trajectories
+    '''
     t = get_template('index2.html')
     html = t.render()
     return HttpResponse(html)
-def get_trajectories(request):
-    trajectories = get_file("visualize/trajectories.txt")
-    return JsonResponse(trajectories)
-# Create your views here.
-def get_file(filename):
-    d = collections.defaultdict(dict)
 
+def get_trajectories(request):
+    '''
+    returns the trajectory data in the text file in JSON format.
+    '''
+    trajectories = get_file("visualize/trajectories.txt")  
+    return JsonResponse(trajectories)
+    #TODO: Right now, we are only returning a hard coded trajectory data. We should expand this to allow users to choose
+    #       from a list of trajectory data
+
+def get_file(filename):
+    '''
+    returns a dictionary of each trajectories coordinates for every time stamp from the netlogo input format data 
+    '''
+    d = collections.defaultdict(dict)
     with open(os.path.join(settings.BASE_DIR, filename), 'r') as f:
-        line_generator = [line for line in f]
-        n = int(line_generator[0])
-        tau = int(line_generator[1])
-        trajectory_id = 1;
-        t = 0;
-        idx = 2;
+        lines = [line for line in f]
+        n = int(lines[0]) #number of trajectories
+        tau = int(lines[1]) #number of time steps for the trajectories
+        trajectory_id = 1
+        t = 0
+        idx = 2
         while True:
-            #print (idx)
-           
-            if(t == tau):                
+            if(t == tau):  #start new trajectory              
                 t = 0
                 trajectory_id +=1
             if (trajectory_id > n):
                     break                
-            coordinate = line_generator[idx].replace('[', '').replace(']','').split()
+            coordinate = lines[idx].replace('[', '').replace(']','').split() 
             d[trajectory_id][t] = coordinate            
             t+=1
             idx+=1
